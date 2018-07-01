df671fd1da07c9cdf78e933502b350e9b9a70c78
Scarlett Perry <si.perry15@gmail.com>
2018-07-01 19:08:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   array.join
   elsif array.size == 2 
   array.join(" and ")
-  elsif array.size == 3
+  elsif array.size >= 3
   array.join(", ")
   elsif array.size > 3
   array.join()
